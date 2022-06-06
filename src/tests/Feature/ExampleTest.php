d79d10747963cd9bbb852eb45e1ed9a312b0bd0b
Hồng Nhung <1812816@dlu.edu.vn>
2022-06-06 12:54:18 +0700

    Add database slider fix image

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -14,8 +14,22 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/admin');
+        $response->assertStatus(200);
+    }
 
+    public function testResponseHome() {
+        $response = $this->get('/home');
+        $response->assertStatus(200);
+    }
+
+    public function testAdminCategories() {
+        $response = $this->get('/admin/categories');
+        $response->assertStatus(200);
+    }
+
+    public function testAdminCreate() {
+        $response = $this->get('/admin/categories/create');
         $response->assertStatus(200);
     }
 }
